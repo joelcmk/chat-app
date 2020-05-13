ff2c96e4335a4c639f3e548da41f7337e7f6b5a2
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-05-12 21:36:58 -0500

    Added buttons and style to start screen

--- components/Start.js
+++ components/Start.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Text, Button, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ImageBackground } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+import { Button } from 'react-native-elements';
 
 
 
@@ -8,23 +9,58 @@ export default class Screen1 extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { text: '' }
+    this.state = {
+      text: '',
+
+    }
   }
 
   render() {
     return (
-      <View style={styles.cotainer}>
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => this.setState({ text })}
-          value={this.state.text}
-          placeholder='Enter your name'
-        />
-        <Button
-          title="Start Chatting"
-          onPress={() => this.props.navigation.navigate('Screen2')}
-        />
-      </View>
+      <ImageBackground source={require('../assets/Background-Image.png')} style={{ width: '100%', height: '100%' }} >
+        <View style={styles.container}>
+          <Text>Chat App</Text>
+          <View style={styles.box}>
+            <TextInput
+              style={styles.input}
+              onChangeText={(text) => this.setState({ text })}
+              value={this.state.text}
+              placeholder='Your Name'
+            />
+            <Text>Choose Background Color</Text>
+            <View style={styles.color}>
+              <Button
+                title=''
+                onPress={() => this.props.navigation.navigate('Screen2')}
+                buttonStyle={styles.color1}
+              />
+              <Button
+                title=''
+                onPress={() => this.props.navigation.navigate('Screen2')}
+                buttonStyle={styles.color2}
+              />
+              <Button
+                title=''
+                onPress={() => this.props.navigation.navigate('Screen2')}
+                buttonStyle={styles.color3}
+              />
+              <Button
+                title=''
+                onPress={() => this.props.navigation.navigate('Screen2')}
+                buttonStyle={styles.color4}
+              />
+            </View>
+            <View style={styles.button_start}>
+              <Button
+                buttonStyle={styles.button_start}
+                color='black'
+                title="Start Chatting"
+                onPress={() => this.props.navigation.navigate('Screen2')}
+              />
+            </View>
+          </View>
+        </View>
+      </ImageBackground >
     );
   }
 }
@@ -33,15 +69,66 @@ export default class Screen1 extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'flex-end',
 
   },
+  box: {
+    backgroundColor: 'white',
+    height: 300,
+    width: 320,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 50,
+  },
   input: {
-    width: '85%',
+    textAlign: 'center',
+    width: 300,
     height: 40,
     borderColor: 'gray',
     borderWidth: 1
+  },
+  color: {
+    flexDirection: "row",
+    margin: 20,
+  },
+  color1: {
+    backgroundColor: '#090C08',
+    width: 50,
+    height: 50,
+    borderRadius: 50 / 2,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  color2: {
+    backgroundColor: '#474056',
+    width: 50,
+    height: 50,
+    borderRadius: 50 / 2,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  color3: {
+    backgroundColor: '#8A95A5',
+    width: 50,
+    height: 50,
+    borderRadius: 50 / 2,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  color4: {
+    backgroundColor: '#B9C6AE',
+    width: 50,
+    height: 50,
+    borderRadius: 50 / 2,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  button_start: {
+    width: 300,
+    height: 40,
+    backgroundColor: '#090C08'
   }
+
 })
